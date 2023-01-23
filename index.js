f95912490a488f4649856b6185708ac5744a8c3f
alam.jahidul <alam.jahidul@genweb2.com>
2023-01-23 12:16:20 +0600

    Add balance check

--- index.js
+++ index.js
@@ -1,12 +1,31 @@
+require('dotenv').config() // dotenv import
+const { default: axios } = require('axios');
+const { response } = require('express');
 const express = require('express')
+const qs = require('qs');
+
+
+// express configure
 const app = express()
 
+
 app.get('/', function (req, res) {
-  res.send('Hello World')
+    res.send('Hello World')
 })
 
 app.get('/balance', function (req, res) {
-    res.send('Hello World')
+    const param = {
+        email: process.env.SMS_API_USER,
+        password: process.env.SMS_API_PASSWORD,
+        method: process.env.SMS_API_GET_BALANCE
+    }
+    axios.post(process.env.SMS_API_URL,qs.stringify(param))
+        .then(response => {
+            res.json(response.data)
+        })
+        .catch(error => {
+            res.json(error)
+        }) 
 })
 
 app.listen(4000)
