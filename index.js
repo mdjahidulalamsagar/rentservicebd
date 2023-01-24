4b30ac0d2dd642b2e3c069d4cfa10b1cd42c937c
alam.jahidul <alam.jahidul@genweb2.com>
2023-01-24 10:43:19 +0600

    Add nodeCache in otp

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const { response } = require('express');
 const express = require('express')
 const bodyParser = require('body-parser')
 const qs = require('qs');
+const NodeCache = require("node-cache");
+const myCache = new NodeCache();
 
 const Bearer = require('./Bearer')
 const OTP = require('./GenerateOtp');
@@ -17,19 +19,64 @@ app.use(bodyParser.json())
 app.get('/', function (req, res) {
     res.send('Hello World')
 })
-app.get('/otp', (req, res) => {
-    const mobileNumber = req.body.mobile;
+app.get('/cache', (req, res) => {
+    const { mobile } = req.body.mobile;
+    console.log(mobile)
+    if (!mobile) {
+        return res.json({ message: 'provide a mobile number' })
+    }
+    console.log('before', myCache.get(mobile))
+    if (!myCache.get(mobile)) {
+        console.log('set')
+        myCache.set(mobile, mobile, 60);
+    }
+    console.log('after', myCache.get(mob))
+    console.log(myCache.getTtl(mob))
+    res.json({ ok: 'ok' })
+})
+app.post('/otp', (req, res) => {
+    /*
+    check if user already registered or not.
+    if yes return with message
+    if not then follow procedure 
+    */
+    const { mobile } = req.body;
     const otpNumber = OTP.otp();
-    const param = qs.stringify(SingleMessage("আপনার OTP নাম্বার " + otpNumber,mobileNumber));
+    const param = qs.stringify(SingleMessage("আপনার OTP নাম্বার " + otpNumber, mobile));
+    
+    // if prameter not exist
+    if (!mobile) {
+        return res.json({
+            status: 'failed',
+            message: 'please provide a mobile number'
+        })
+    }
+    // first check if mobile number exist
+    if (myCache.get(mobile)) {
+        return res.json({
+            status: 'unseccussful',
+            message: 'wait for otp',
+            time: new Date(myCache.getTtl(mobile)).toString()
+        })
+    }
+    // if not then set & send & store in db this message info
     axios.post(process.env.SMS_API_URL, param)
         .then(response => {
-            console.log("status ",response.status)
-            console.log("data",response.data)
+            console.log(response.data)
+            if (response.data.error_code === 0) {
+                myCache.set(mobile, mobile, 60);
+                return res.json({
+                    status: 'success',
+                    message: 'otp is being sent'
+                })
+            }
         })
         .catch(error => {
-            console.log(error)
+            return res.json({
+                status: 'failed',
+                message: 'error happens'
+            })
         })
-    res.json(otpNumber)
 })
 
 app.get('/balance', function (req, res) {
